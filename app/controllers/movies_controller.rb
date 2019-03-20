3d5bc745656f6a5b9cc7b613c97fc06006c2bfe6
sylwuskak <sylwuskak@gmail.com>
2019-03-20 14:26:40 +0100

    SEP Task 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,50 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:send_info]
+  require 'net/http'
+  require 'json'
+
 
   def index
     @movies = Movie.all.decorate
+<<<<<<< HEAD
+=======
+    @movies_titles = @movies.map{|m| m.title}.uniq
+    @movies_data = @movies_titles.map do |title|
+      [title, movie_data(title.gsub(' ', ''))]
+    end.to_h
+
+    respond_to do |format|
+      movies_json = @movies.map do |movie|
+        {
+          id: movie.id,
+          title: movie.title
+        }
+      end.to_json
+
+      format.html
+      format.json do
+        render json: movies_json
+      end
+    end
+>>>>>>> 15534dd... SEP Task 1
   end
 
   def show
     @movie = Movie.find(params[:id])
+<<<<<<< HEAD
+=======
+    @movie_data = movie_data(@movie.title.gsub(' ', ''))
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          id: @movie.id,
+          title: @movie.title
+        }.to_json
+      end
+    end
+>>>>>>> 15534dd... SEP Task 1
   end
 
   def send_info
@@ -19,5 +57,18 @@ class MoviesController < ApplicationController
     file_path = "tmp/movies.csv"
     MovieExporter.new.call(current_user, file_path)
     redirect_to root_path, notice: "Movies exported"
+  end
+
+  private
+  def movie_data(title)
+    url = "https://pairguru-api.herokuapp.com/api/v1/movies/" + title
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    @movie_data = JSON.parse(response).try(:[], "data").try(:[], "attributes")
+    if @movie_data 
+      @movie_data["poster_image"] = "https://pairguru-api.herokuapp.com" + @movie_data["poster"]
+    end
+    @movie_data
   end
 end
