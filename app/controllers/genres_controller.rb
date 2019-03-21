f7abdc746c0e7d0cd529927a3568797e11aa38e4
sylwuskak <sylwuskak@gmail.com>
2019-03-21 21:12:40 +0100

    fix bugs

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -11,6 +11,7 @@ class GenresController < ApplicationController
         }
       end.to_json
 
+      format.html
       format.json do
         render json: genres_json
       end
@@ -20,5 +21,22 @@ class GenresController < ApplicationController
 
   def movies
     @genre = Genre.find(params[:id]).decorate
+    @movies_titles = @genre.movies.map{|m| m.title}.uniq
+    @movies_data = @movies_titles.map do |title|
+      [title, movie_data(title.gsub(' ', ''))]
+    end.to_h
+  end
+
+  private
+  def movie_data(title)
+    url = "https://pairguru-api.herokuapp.com/api/v1/movies/" + title
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    @movie_data = JSON.parse(response).try(:[], "data").try(:[], "attributes")
+    if @movie_data 
+      @movie_data["poster_image"] = "https://pairguru-api.herokuapp.com" + @movie_data["poster"]
+    end
+    @movie_data
   end
 end
